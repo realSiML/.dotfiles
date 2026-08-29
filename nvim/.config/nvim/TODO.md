e717c51739a69061d7be29e84df0bf8f77b89678
Artyom Kim <arti-kim@yandex.ru>
2026-08-29 19:08:02 +1100

    remove: nvim to add as submodule

--- nvim/.config/nvim/TODO.md
+++
@@ -1,5 +0,0 @@
-# TODO
-
-- Extend lua/plugins/mini.lua
-- Figure out lua/keymaps.lua
-- Try wansmer/langmapper.nvim
--- nvim/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require("vim._core.ui2").enable({})
-
-require('options')
-require('keymaps')
-require('config.lazy')
-
-vim.cmd("colorscheme rose-pine-moon")
